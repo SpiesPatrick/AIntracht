6018b6bd64084ea0a7cdaa5b12e2e5d227dd3d1f
Postman <patrick_post@gmx.de>
2025-09-04 21:48:35 +0200

    extended the prompt generator

--- backend-py/prompt.py
+++ backend-py/prompt.py
@@ -1,21 +1,151 @@
 
-PROMPT_TEMPLATE = """
-Gib mir bitte die Tipps für den {spieltag}. Spieltag der 1. Bundesliga im YAML-Format.
+import json
+from datetime import datetime
 
-Die Struktur soll exakt so aussehen:
+import requests
 
-saison:
-  - spieltag: "{spieltag}"
-    begegnungen:
-      - Heimmannschaft: "Tore"
-        Gastmannschaft: "Tore"
-    - ...
 
-Wichtige Regeln:
-- Alle 9 Spiele des Spieltags müssen enthalten sein.
-- Nur YAML zurückgeben, keine zusätzlichen Erklärungen oder Kommentare.
-- Alle Werte (auch Zahlen) immer in Anführungszeichen ("...").
-"""
+def get_table():
+    '''
+    return a json or yaml (not sure yet) of the actual table situation
+    '''
+    year = datetime.now().year if datetime.now().month >= 7 else datetime.now().year - 1
+    response = requests.get(f'https://api.openligadb.de/getbltable/bl1/{year}')
+    data = response.json()
+    reduced_data = [
+        {
+            'team': item.get('teamName'),
+            'punkte': item.get('points'),
+            'siege': item.get('won'),
+            'niederlagen': item.get('lost'),
+            'unentschieden': item.get('draw'),
+            'tore_geschossen': item.get('goals'),
+            'tore_gegner': item.get('opponentsGoals'),
+            'tordifferenz': item.get('goalDiff')
+        }
+        for item in data
+    ]
+    return json.dumps(reduced_data, indent=2, ensure_ascii=False)
 
-def get_prompt_for_spieltag(spieltag):
-    return PROMPT_TEMPLATE.format(spieltag=spieltag)
+def get_match_day():
+    '''
+    return integer representing the next matchday
+    '''
+    response = requests.get('https://api.openligadb.de/getmatchdata/bl1')
+    data = response.json()
+    return data[0]['group']['groupOrderID']
+
+def get_team_names():
+    '''
+    return string with all team names, the way I want (and need) it
+    '''
+    return  '  - "Hamburger SV"\n' \
+            '  - "FC St. Pauli"\n' \
+            '  - "RB Leipzig"\n' \
+            '  - "1. FC Heidenheim 1846"\n' \
+            '  - "VfB Stuttgart"\n' \
+            '  - "Bor. Mönchengladbach"\n' \
+            '  - "Werder Bremen"\n' \
+            '  - "Bayer 04 Leverkusen"\n' \
+            '  - "1899 Hoffenheim"\n' \
+            '  - "Eintracht Frankfurt"\n' \
+            '  - "FC Augsburg"\n' \
+            '  - "FC Bayern München"\n' \
+            '  - "VfL Wolfsburg"\n' \
+            '  - "FSV Mainz 05"\n' \
+            '  - "Borussia Dortmund"\n' \
+            '  - "1. FC Union Berlin"\n' \
+            '  - "1. FC Köln"\n' \
+            '  - "SC Freiburg"\n' \
+
+def generate_promt():
+    '''
+    return the complete prompt
+    '''
+    match_day = get_match_day()
+    team_names = get_team_names()
+    table = get_table()
+
+    return f'Gib mir bitte die Tipps für den {match_day}. Spieltag der 1. Bundesliga im YAML-Format.\n' \
+            'Verwende genau die folgenden Team-Namen (keine Abweichungen, keine Kürzungen):\n' \
+            '\n' \
+            'teams:\n' \
+           f'{team_names}\n' \
+            '\n' \
+            'Das YAML soll so aufgebaut sein:\n' \
+            '\n' \
+            'saison:\n' \
+            '  jahr: "25/26"\n' \
+            '  spiele:\n' \
+            '  - spieltag: 3 # Integer\n' \
+            '    begegnungen:\n' \
+            '    - heim_mannschaft: "FC Bayern München" # Name der Heimmannschaft\n' \
+            '      gast_mannschaft: "Werder Bremen" # Name der Gastmannschaft\n' \
+            '      heim_tore: 0 # Integer\n' \
+            '      gast_tore: 2 # Integer\n' \
+            '    - ...\n' \
+            '\n' \
+            'Die aktuelle Tabelle der 1. Bundesliga sieht wie folgt aus (JSON-Format):\n' \
+           f'{table}\n' \
+            '\n' \
+            '\n' \
+            'Wichtige Punkte:\n' \
+            '- Alle Spiele des Spieltags müssen enthalten sein.\n' \
+            '- Team-Namen exakt so wie oben in der Liste (keine Variationen).\n' \
+            '\n' \
+
+def main():
+    prompt = generate_promt()
+    print(prompt)
+
+if __name__ == '__main__':
+    main()
+
+
+# PROMPT_TEMPLATE = """
+# Gib mir bitte die Tipps für den {spieltag}. Spieltag der 1. Bundesliga im YAML-Format.
+# Verwende genau die folgenden Team-Namen (keine Abweichungen, keine Kürzungen):
+
+# teams:
+#   - "Hamburger SV"
+#   - "FC St. Pauli"
+#   - "RB Leipzig"
+#   - "1. FC Heidenheim 1846"
+#   - "VfB Stuttgart"
+#   - "Bor. Mönchengladbach"
+#   - "Werder Bremen"
+#   - "Bayer 04 Leverkusen"
+#   - "1899 Hoffenheim"
+#   - "Eintracht Frankfurt"
+#   - "FC Augsburg"
+#   - "FC Bayern München"
+#   - "VfL Wolfsburg"
+#   - "FSV Mainz 05"
+#   - "Borussia Dortmund"
+#   - "1. FC Union Berlin"
+#   - "1. FC Köln"
+#   - "SC Freiburg"
+
+# Das YAML soll so aufgebaut sein:
+
+# saison:
+#   jahr: '25/26'
+#   spiele:
+#   - spieltag: 3 # Integer
+#     begegnungen:
+#     - heim_mannschaft: 'FC Bayern München' # Name der Heimmannschaft
+#       gast_mannschaft: 'Werder Bremen' # Name der Gastmannschaft
+#       heim_tore: 0 # Integer
+#       gast_tore: 2 # Integer
+#     - ...
+
+
+# Wichtige Punkte:
+# - Alle Spiele des Spieltags müssen enthalten sein.
+# - Team-Namen exakt so wie oben in der Liste (keine Variationen).
+# """
+
+# def get_prompt_for_spieltag(spieltag):
+#     return PROMPT_TEMPLATE.format(spieltag=spieltag)
+# def get_prompt_for_spieltag(spieltag):
+#     return PROMPT_TEMPLATE.format(spieltag=spieltag)
