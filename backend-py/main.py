ca73485c37705070d6ab7c9e7b445bbcddf65406
Postman <patrick_post@gmx.de>
2025-09-05 22:35:43 +0200

    using config, change marks, handle exceptions

--- backend-py/main.py
+++ backend-py/main.py
@@ -1,47 +1,56 @@
-import os
 
+import config
 import google.generativeai as genai
 import prompt
 import yaml
 from google.generativeai.types import HarmCategory
 
-api_key=os.getenv("GEMINI_API_KEY")
-if not api_key:
-    raise ValueError("GEMINI_API_KEY wurde nicht in den Umgebungsvariablen gefunden.")
-
-genai.configure(api_key=api_key)
-
 
 def get_bundesliga_tips(spieltag: int):
-    """
+    '''
     Holt Bundesliga-Tipps vom Gemini-Modell und gibt sie als JSON zurück.
-    """
+    '''
+    conf = config.load_config()
 
-    model = genai.GenerativeModel('gemini-2.0-flash')
+    api_key = conf.gemini.api_key
+    if not api_key:
+        raise ValueError('GEMINI_API_KEY wurde nicht in der Config gefunden.')
+
+    genai.configure(api_key=api_key)
+
+    bot_model = conf.gemini.bot_model
+    if not bot_model:
+        raise ValueError('BOT-MODEL wurde nicht in der Config gefunden.')
+
+    model = genai.GenerativeModel(bot_model)
 
     try:
+        p = prompt.generate_promt()
+        if not p:
+            raise NotImplementedError('Prompt wurde nicht generiert')
+
         response = model.generate_content(prompt.generate_promt())
 
         if not response.parts:
-            raise ValueError(f"Antwort ist leer. Finish Reason: {response.candidates[0].finish_reason}")
+            raise ValueError(f'Antwort ist leer. Finish Reason: {response.candidates[0].finish_reason}')
 
         text = response.text.strip()
 
         if not text:
-            raise ValueError("Antwort vom Modell war leer.")
+            raise ValueError('Antwort vom Modell war leer.')
 
-        if text.startswith("```yaml"):
-            text = text.strip("```yaml").strip()
+        if text.startswith('```yaml'):
+            text = text.strip('```yaml').strip()
 
         # tips = yaml.dump(data=text, indent=2)
         tips = text
 
     except AttributeError:
-        print("Konnte nicht auf .text zugreifen. Möglicherweise wurde die Anfrage blockiert.")
-        print("Komplette Antwort:", response)
-        raise ValueError("Kein valider Text in der Antwort gefunden.")
+        print('Konnte nicht auf .text zugreifen. Möglicherweise wurde die Anfrage blockiert.')
+        print('Komplette Antwort:', response)
+        raise ValueError('Kein valider Text in der Antwort gefunden.')
     except Exception as e:
-        print(f"Ein unerwarteter Fehler ist aufgetreten: {e}")
+        print(f'Ein unerwarteter Fehler ist aufgetreten: {e}')
         raise
 
     return tips
@@ -52,7 +61,7 @@ def main():
         tips = get_bundesliga_tips(spieltag=1)
         print(tips)
     except Exception as e:
-        print("\nSkript konnte nicht erfolgreich ausgeführt werden.")
+        print('Skript konnte nicht erfolgreich ausgeführt werden: ')
         print(e)
 
 if __name__ == "__main__":
