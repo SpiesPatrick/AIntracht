a48616dca264c899ef2af72990466c00a218c262
Postman <patrick_post@gmx.de>
2025-09-05 22:22:08 +0200

    delete unnecessary files

--- backend-py/bash-curl.sh
+++
@@ -1,5 +0,0 @@
-curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent" \
-  -H 'Content-Type: application/json' \
-  -H "X-goog-api-key: $GEMINI_API_KEY" \
-  -X POST \
-  -d @prompt.json
