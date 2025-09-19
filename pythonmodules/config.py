e1609baf5dc5f8b58558e513e3c0528497927a57
Postman <patrick_post@gmx.de>
2025-09-19 19:47:53 +0200

    fuse configs

--- pythonmodules/config.py
+++ pythonmodules/config.py
@@ -4,17 +4,29 @@ import yaml
 from pydantic import BaseModel
 
 
+class User(BaseModel):
+    name: str
+    e_mail: str
+    password: str
+
+class Kicktipp(BaseModel):
+    group_name: str
+    saison_id: str
+
 class Gemini(BaseModel):
     api_key: str
     bot_model: str
+
 class Config(BaseModel):
     gemini: Gemini
+    user: User
+    kicktipp: Kicktipp
 
 def load_config() -> Config:
     script_path = Path(__file__).resolve()
     project_root = script_path.parent.parent
-    config_path = project_root / "config.yaml"
+    config_path = project_root / 'config.yaml'
 
-    with open(config_path, "r") as f:
+    with open(config_path, 'r') as f:
         data = yaml.safe_load(f)
     return Config(**data)
