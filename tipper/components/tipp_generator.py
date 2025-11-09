5b6974b02a96ca60f2230b14ce7212b4c480c1df
Postman <patrick_post@gmx.de>
2025-11-09 12:49:20 +0100

    new structure 2.0

---
+++ tipper/components/tipp_generator.py
@@ -0,0 +1,95 @@
+
+import google.generativeai as genai
+import yaml
+from google.generativeai.types import HarmCategory
+# import pythonmodules.config as config
+from models import config
+from services.datacon import Datacon
+from services.prompt import Prompt
+
+
+def get_bundesliga_tips(conf: config.Config, prompt: Prompt):
+    api_key = conf.gemini.api_key
+    if not api_key:
+        raise ValueError('GEMINI_API_KEY wurde nicht in der Config gefunden.')
+
+    genai.configure(api_key=api_key)
+
+    bot_model = conf.gemini.bot_model
+    if not bot_model:
+        raise ValueError('BOT-MODEL wurde nicht in der Config gefunden.')
+
+    model = genai.GenerativeModel(bot_model)
+
+    try:
+        # Generiere den Prompt
+        p = prompt.generate_prompt()
+        if not p:
+            raise NotImplementedError('Prompt wurde nicht generiert')
+
+        response = model.generate_content(p)
+
+        if not response.parts:
+            raise ValueError(f'Antwort ist leer. Finish Reason: {response.candidates[0].finish_reason}')
+
+        text = response.text.strip()
+
+        if not text:
+            raise ValueError('Antwort vom Modell war leer.')
+
+        if text.startswith('```yaml'):
+            text = text.strip('```yaml').strip()
+
+        # tips = yaml.dump(data=text, indent=2)
+        tips = text
+
+    except AttributeError:
+        print('Konnte nicht auf .text zugreifen. Möglicherweise wurde die Anfrage blockiert.')
+        print('Komplette Antwort:', response)
+        raise ValueError('Kein valider Text in der Antwort gefunden.')
+    except Exception as e:
+        print(f'Ein unerwarteter Fehler ist aufgetreten: {e}')
+        raise
+
+    return tips
+
+def safe_bundesliga_tips_into_db(tips):
+    # @TODO continue here
+    pass
+
+def main():
+
+    datacon = Datacon()
+    prompt = Prompt()
+
+    try:
+        conf = config.load_config()
+    except:
+        print('Unable to load config')
+        exit()
+
+    try:
+        conn = datacon.connection(dbname=conf.postgres.db_name, user=conf.postgres.user_name, password=conf.postgres.password, host=conf.postgres.host)
+    except:
+        print('Unable to connect to database')
+        exit()
+
+    saison_year = prompt.get_saison_year()
+    match_day = prompt.get_match_day()
+
+    with conn.cursor() as cur:
+        if datacon.check_if_spieltag_and_saison_already_exists(cur=cur, jahr=saison_year, spieltag=match_day):
+            # @TODO Logging
+            print('"Spieltag" in this saison already exists in database')
+            exit()
+        try:
+            tips = get_bundesliga_tips(conf=conf)
+            print(tips)
+            safe_bundesliga_tips_into_db(tips)
+        except Exception as e:
+            print('Skript konnte nicht erfolgreich ausgeführt werden: ')
+            print(e)
+
+if __name__ == "__main__":
+    main()
+    main()
---
+++ tipper/components/tipp_sender.py
@@ -0,0 +1,115 @@
+
+import re
+import time
+
+import models.config as conf
+import models.tipps as gen_tipps
+import yaml
+from playwright.sync_api import expect, sync_playwright
+
+
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False) # True =^ unsichtbar
+        page = browser.new_page()
+
+
+        # Load config
+        try:
+            config = conf.load_config()
+            E_MAIL = config.user.e_mail
+            PASSWORD = config.user.password
+            GROUPNAME = config.kicktipp.group_name
+            SAISON_ID = config.kicktipp.saison_id
+        except Exception as e:
+            print('Failed to load config')
+            return
+
+        # Load Tipps
+        try:
+            tipps = gen_tipps.load_tipp()
+        except Exception as e:
+            print('Failed to load tipps')
+            return
+
+        print(tipps)
+        SPIELE = max(tipps.saison.spiele, key=lambda s: s.spieltag)
+        SPIELTAG = SPIELE.spieltag
+        print(f'current spieltag is {SPIELTAG}')
+
+        page.goto(f'https://www.kicktipp.de/{GROUPNAME}/')
+
+        '''
+        1) Login
+        '''
+        page.get_by_role('link', name='').click()
+        page.get_by_role('textbox', name='E-Mail').click()
+        page.get_by_role('textbox', name='E-Mail').fill(E_MAIL)
+        page.get_by_role('textbox', name='Passwort').click()
+        page.get_by_role('textbox', name='Passwort').fill(PASSWORD)
+        page.get_by_role('button', name='Anmelden').click()
+
+        '''
+        2) Navigate to Tipppage, depending on SPIELTAG
+        '''
+        time.sleep(1)
+        try:
+            page.locator("iframe[title=\"SP Consent Message\"]").content_frame.get_by_role("button", name="Akzeptieren und weiter").click()
+        except Exception:
+            print('No I-Frame found')
+        # page.get_by_role("link", name="Tippabgabe").click()
+        page.goto(f'https://www.kicktipp.de/{GROUPNAME}/tippabgabe?tippsaisonId={SAISON_ID}&spieltagIndex={SPIELTAG}')
+
+        time.sleep(1)
+        try:
+            page.locator("iframe[title=\"SP Consent Message\"]").content_frame.get_by_role("button", name="Akzeptieren und weiter").click()
+        except Exception:
+            print('No I-Frame found')
+
+        '''
+        3) Filling the tips
+        '''
+        rows = page.locator('table#tippabgabeSpiele tbody tr.datarow' )
+
+        count = rows.count()
+
+        for i in range(count):
+            row = rows.nth(i)
+            heim = row.locator('.nw.cell.col1').inner_text()
+            gast = row.locator('.nw.cell.col2').inner_text()
+
+            '''
+            TODO hier muss ich die Tipps/Tore aus meinem YAML auslesen
+            und richtig definieren
+            '''
+            tore_heim = None
+            tore_gast = None
+
+            for match in SPIELE.begegnungen:
+                if match.heim_mannschaft == heim and match.gast_mannschaft == gast:
+                    tore_heim = match.heim_tore
+                    tore_gast = match.gast_tore
+                    pass
+
+            if tore_heim == None or tore_gast == None:
+                print(f'Could not match team for ${heim} or ${gast}')
+                return
+
+            row.locator('input[name*="heimTipp"]').fill(str(tore_heim))
+            row.locator('input[name*="gastTipp"]').fill(str(tore_gast))
+
+        '''
+        4) Submit und check if saved
+        '''
+        page.get_by_role('button', name='Tipps speichern').click()
+        expect(page.get_by_role('paragraph')).to_contain_text([
+            'Die Tipps wurden erfolgreich gespeichert.',
+            'Es wurden keine Änderungen gespeichert! Es wurden die gleichen Daten übermittelt, die bereits gespeichert sind.'
+        ])
+
+def sortBySpieltag(e):
+  return e.spieltag
+
+if __name__ == '__main__':
+    main()
+    main()
