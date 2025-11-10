045885619d19bfbfe37e43dffff2f18974cffea7
Postman <patrick_post@gmx.de>
2025-11-10 06:53:51 +0100

    main method calls two components

--- tipper/main.py
+++ tipper/main.py
@@ -8,14 +8,14 @@ def main():
       -> generate prompt and send it to bot
       -> receive tipps and safe them into DB
     '''
-    tipp_generator
+    tipp_generator.generate()
 
     '''
-    Check if tipps already tipped in DB (bool "getippt"). If not:
+    Check if tipps already safed AND tipped in DB (bool "getippt"). If not:
       -> get tipps from current matchday
       -> navigate through Kicktipp and tipp the shit out of hell
     '''
-    tipp_sender
+    tipp_sender.send()
 
 if __name__ == '__main__':
     main()
